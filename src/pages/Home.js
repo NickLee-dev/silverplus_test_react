fafccf376aec2b4c24c5040c00df02e164d65176
SangMoo Lee <dltkdan7373@hanmail.net>
2024-12-12 16:00:18 +0900

    chore
    이미지 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
+import background from '../assets/images/game/other/background.png';
 
 const Home = () => {
   return (
     <div>
-      <h1 className={styles}>Home</h1>
-      <Link to="/game">맞고시작</Link>
+      <Link to="/game" className="startButton">
+        맞고시작
+      </Link>
     </div>
   );
 };
